8af7e0fc8396e8838dd7b5bbeed1fa87104c6836
GabrielTorbes <gabrieltorbes@gmail.com>
2023-12-07 10:05:22 -0300

    first commit

---
+++ apps/palimentar/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class PalimentarConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'palimentar'
+    verbose_name = 'Palimentar'
